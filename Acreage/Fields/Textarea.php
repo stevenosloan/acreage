8329366b8adbf65df941c357b02caaa32dcfeacd
Steven Sloan <stevenosloan@gmail.com>
2016-01-15 13:28:58 -0500

    cleanup TextArea and add example to the example app

--- Acreage/Fields/Textarea.php
+++ Acreage/Fields/Textarea.php
@@ -6,25 +6,4 @@ use \Acreage\Render;
 
 class Textarea extends Base {
 
-
-  public function render_input() {
-    return Render::tag( 'textarea', $this->attributes() );
-  }
-
-  public function formatted_value() {
-    return explode("\n", $this->value);
-  }
-
-  private function attributes() {
-    $classes = array('form-area');
-    if( $this->valid === false ) {
-      array_push( $classes, 'field-error' );
-    }
-
-    return array(
-      'name'    => $this->name,
-      'class'   => implode(' ', $classes),
-      'content' => $this->value
-    );
-  }
 }
